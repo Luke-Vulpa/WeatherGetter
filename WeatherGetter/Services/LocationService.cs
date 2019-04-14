d7b6516f9360c078236e776f9e1fa0a9148f98fd
Luke Vulpa <lukie@subvulpa.net>
2019-04-14 15:50:42 +0100

    Test Clean up

--- WeatherGetter/Services/LocationService.cs
+++ WeatherGetter/Services/LocationService.cs
@@ -15,8 +15,9 @@ namespace WeatherGetter.Services
     public class LocationService
     {
         /// <summary>
-        ///  Class is used to iterate over siteList and get sites in range of postcode. That data is then used to build UI.
+        ///  Class is used to iterate over siteList and get sites in range of postcode. That data is then used to build the UI.
         /// </summary>
+        /// 
         public enum DistanceType { Miles, Kilometers };
         
         public Location[] ValidLocations;
@@ -31,7 +32,7 @@ namespace WeatherGetter.Services
         {
 
             // get data from sitelist.json
-            Locations locations = createModel();
+            Locations locations = createModel(Resources.sitelist2);
             Location[] filtered = new Location[500];
 
             int counter = 0;
@@ -47,19 +48,19 @@ namespace WeatherGetter.Services
                 
             }
 
-            //instanciate array trimming null values from filter.
+            //trimming null values
             this.ValidLocations = filtered.Where(loc => loc != null).ToArray();
 
             // returned list is used for populating buttons to gain further location information
 
         }
 
-        public Locations createModel()
+        private Locations createModel(string json)
         {
             try
             {
 
-                var rootObject = JsonConvert.DeserializeObject<RootObject>(Resources.sitelist2);
+                var rootObject = JsonConvert.DeserializeObject<RootObject>(json);
                 var tempLocations = rootObject.Locations;
 
                 return tempLocations;
@@ -76,38 +77,6 @@ namespace WeatherGetter.Services
         }
 
         
-        //hacky remove later
-        public static double DistanceBetweenLocations(Coordinate location1, Coordinate location2)
-        {
-            //D = sqrt((Ax - Bx)2 + (Ay - By)2)
-
-            double distance = Math.Sqrt(
-                                        Math.Pow((location1.Longitude - location2.Longitude), 2) +
-                                        Math.Pow((location1.Latitude - location2.Latitude), 2));
-
-            return distance * 69;
-            // 6.35466244751442
-        }
-
-        // is this needed anymore?
-        public static double Distance(Coordinate pos1, Coordinate pos2, DistanceType type)
-        {
-            //https://gist.github.com/jammin77/033a332542aa24889452
-
-            double R = (type == DistanceType.Miles) ? 3960 : 6371;
-
-            double dLat = toRadian(pos2.Latitude - pos1.Latitude);
-            double dLon = toRadian(pos2.Longitude - pos1.Longitude);
-
-            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
-                Math.Cos(toRadian(pos1.Latitude)) * Math.Cos(toRadian(pos2.Latitude)) *
-                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
-            double c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
-            double d = R * c;
-
-            return d;
-        }
-
         //override to deal with location data
         public static double Distance(Coordinate pos1, double pos2lat, double pos2long, DistanceType type)
         {
--- WeatherGetterTests/LocationServiceTests.cs
+++ WeatherGetterTests/LocationServiceTests.cs
@@ -14,49 +14,21 @@ namespace Tests
 
         }
 
-        [Test]
-        public void DistanceBetweenLocationsTest()
-        {
-            var sutAirport = new Coordinate(50.7354, -3.4201);
-            var sutHome = new Coordinate(50.731525, -3.512115);
-            /*
-            "latitude": "50.7354",
-            "longitude": "-3.4201",
-            "name": "Exeter International Airport",  
-
-            "latitude": "50.731525",
-            "longitude": "-3.512115",
-            "name": " home ",
-
-            */
-
-            //Arrange
-
-
-
-            double result = LocationService.DistanceBetweenLocations(sutAirport, sutHome);
-            Assert.Pass(result.ToString());
-        }
 
         [Test]
-        public void DistanceTest()
+        public void Distance_LessThan10()
         {
             var sutAirport = new Coordinate(50.7354, -3.4201);
             var sutHome = new Coordinate(50.731525, -3.512115);
 
-            var result = LocationService.Distance(sutAirport, sutHome, LocationService.DistanceType.Miles);
-            Assert.Pass(result.ToString());
+            var result = LocationService.Distance(sutAirport, sutHome.Latitude, sutHome.Longitude, LocationService.DistanceType.Miles);
+            //Assert.Pass(result < 10);
+
+            Assert.LessOrEqual(result, 10);
 
 
         }
 
-        [Test]
-        public void IterateOverSiteList()
-        {
-            var file = WeatherGetter.Properties.Resources.sitelist;
-            var sut = new LocationService();
-            sut.createModel();
-            
-        }
+ 
     }
 }
